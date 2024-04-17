1498c1c04c64dea8eddb373592f68c045f3da845
stephen ijeh <stephencode@hotmail.com>
2024-04-17 21:00:34 +0100

    Add mobile and desktop nav

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Route, Routes} from "react-router-dom";
+import Layout from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<span>HOME PAGE</span>} />
+      <Route path="/" element={<Layout><HomePage/></Layout>} />
       <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
